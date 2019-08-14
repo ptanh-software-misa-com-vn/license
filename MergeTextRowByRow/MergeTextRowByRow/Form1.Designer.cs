7f975ebbcb69972fa46ff90017ecb8926fd8d43b
phamtrananh <phamtrananh1088@gmail.com>
2019-08-14 15:13:35 +0900

    master MergeTextRowByRow

---
+++ MergeTextRowByRow/MergeTextRowByRow/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace MergeTextRowByRow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label2 = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.tableLayoutPanel2.SuspendLayout();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.textBox1.Location = new System.Drawing.Point(3, 3);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox1.Size = new System.Drawing.Size(490, 305);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.Text = resources.GetString("textBox1.Text");
+			// 
+			// textBox2
+			// 
+			this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.textBox2.Location = new System.Drawing.Point(499, 3);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox2.Size = new System.Drawing.Size(491, 305);
+			this.textBox2.TabIndex = 1;
+			this.textBox2.Text = resources.GetString("textBox2.Text");
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(993, 311);
+			this.tableLayoutPanel1.TabIndex = 2;
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.ColumnCount = 1;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel2.Controls.Add(this.textBox3, 0, 2);
+			this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 0);
+			this.tableLayoutPanel2.Controls.Add(this.panel1, 0, 1);
+			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 3;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(999, 666);
+			this.tableLayoutPanel2.TabIndex = 3;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.textBox3.Location = new System.Drawing.Point(3, 352);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox3.Size = new System.Drawing.Size(993, 311);
+			this.textBox3.TabIndex = 7;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.numericUpDown1);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(3, 320);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(993, 26);
+			this.panel1.TabIndex = 8;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(426, 4);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(20, 12);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "ms";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(369, 2);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(51, 19);
+			this.numericUpDown1.TabIndex = 2;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(539, 4);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 12);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "label1";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(457, 0);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Merge Text";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(999, 666);
+			this.Controls.Add(this.tableLayoutPanel2);
+			this.Name = "Form1";
+			this.Text = "Merge Text Row By Row";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.tableLayoutPanel2.ResumeLayout(false);
+			this.tableLayoutPanel2.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MergeTextRowByRow/MergeTextRowByRow/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Configuration;
+
+namespace MergeTextRowByRow
+{
+	public partial class Form1 : Form
+	{
+		private int _SpaceLen = string.IsNullOrEmpty(ConfigurationManager.AppSettings.Get("SpaceLen")) ? 64 : int.Parse(ConfigurationManager.AppSettings.Get("SpaceLen"));
+		private bool _SpaceOn = string.IsNullOrEmpty(ConfigurationManager.AppSettings.Get("SpaceOn")) ? true : bool.Parse(ConfigurationManager.AppSettings.Get("SpaceOn"));
+		private char _SplitChar = string.IsNullOrEmpty(ConfigurationManager.AppSettings.Get("SplitChar")) ? '\t' : (char)int.Parse(ConfigurationManager.AppSettings.Get("SplitChar"));
+		private bool _CSV = string.IsNullOrEmpty(ConfigurationManager.AppSettings.Get("CSV")) ? false : bool.Parse(ConfigurationManager.AppSettings.Get("CSV"));
+		public Form1()
+		{
+			InitializeComponent();
+			Init();
+		}
+
+		private void Init()
+		{
+			label1.Text = "";
+			label1.ForeColor = Color.Black;
+			textBox3.ForeColor = Color.Black;
+			textBox3.Text = "";
+		}
+
+		private string _sResult = "";
+
+		private async void button1_Click(object sender, EventArgs e)
+		{
+			Init();
+			IEnumerator<string> Text1 = GetEnume(textBox1.Text).GetEnumerator();
+			IEnumerator<string> Text2 = GetEnume(textBox2.Text).GetEnumerator();
+			Timer tim = new Timer();
+			tim.Tick += new EventHandler(tim_Tick);
+			tim.Interval = (int)numericUpDown1.Value;
+			tim.Start();
+			Func<string> func = () =>
+								{
+									StringBuilder sbText = new StringBuilder();
+									bool hasRow1 = true;
+									bool hasRow2 = true;
+									while (hasRow1 || hasRow2)
+									{
+										hasRow1 = Text1.MoveNext();
+										hasRow2 = Text2.MoveNext();
+										System.Threading.Thread.Sleep((int)numericUpDown1.Value);
+										if (hasRow1 || hasRow2)
+										{
+											sbText.AppendLine((hasRow1 ? Text1.Current : "") + GetFeedTab(hasRow1 ? Text1.Current.Length : 0, _SpaceLen) + (hasRow2 ? Text2.Current : "") + (_CSV ? _SplitChar.ToString() : ""));
+										}
+										_sResult = sbText.ToString();
+									}
+									tim.Tick -= new EventHandler(tim_Tick);
+									tim.Stop();
+									return sbText.ToString();
+								};
+
+			string x = await Task.Factory.StartNew(func);
+			label1.Text = "Xong.";
+			label1.ForeColor = Color.Black;
+			textBox3.Text = x;
+			textBox3.ForeColor = Color.Black;
+		}
+
+		public class MergeTextEventArgs : EventArgs
+		{
+			public MergeTextEventArgs(string text) : base()
+			{
+				this.Text = text;
+			}
+
+			public string Text { get; private set; }
+		}
+
+		private void tim_Tick(object sender, EventArgs e)
+		{
+			if (label1.Text == "Vui lòng đợi...")
+			{
+				label1.Text = "Vui lòng đợi..";
+				label1.ForeColor = Color.Red;
+			}
+			else if (label1.Text == "Vui lòng đợi..")
+			{
+				label1.Text = "Vui lòng đợi.";
+				label1.ForeColor = Color.Purple;
+			}
+			else
+			{
+				label1.Text = "Vui lòng đợi...";
+				label1.ForeColor = Color.Blue;
+			}
+			textBox3.Text = _sResult;
+			textBox3.ForeColor = Color.Blue;
+		}
+
+		private string GetFeedTab(int lenght, int maxlengt)
+		{
+
+			string res = _SplitChar.ToString();
+			if (_SpaceOn)
+			{
+				if (lenght + 4 < maxlengt)
+				{
+					res = new string(_SplitChar, (maxlengt - lenght) / 4);
+				}
+			}
+			return res;
+		}
+		private IEnumerable<string> GetEnume(string text)
+		{
+			using (StringReader read = new StringReader(text))
+			{
+				while (read.Peek() != -1)
+				{
+					yield return read.ReadLine();
+				}
+			}
+		}
+	}
+}
